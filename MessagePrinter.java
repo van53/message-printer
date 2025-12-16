e7c488078b43589419d0102363e2b03fc6ceeef7
qwarden <91963501+van53@users.noreply.github.com>
2025-12-16 22:16:10 +0200

    Revert "Add goodbye message functionality"

--- MessagePrinter.java
+++ MessagePrinter.java
@@ -12,11 +12,5 @@ public class MessagePrinter {
         System.out.println(getWelcomeMessage());
         System.out.println(getDateMessage());
         System.out.println("Learning remote repositories!");
-        System.out.println(getGoodbyeMessage());
     }
-
-    public static String getGoodbyeMessage() {
-        return "Thank you for using MessagePrinter!";
-    }
-
 }
