f7b5e146967a68f7ae450175b132b3f58295f89d
Ivan <ivan.ivanov@kgemt.org.ua>
2025-12-16 19:15:52 +0200

    Add MessagePrinter class with basic functionality

---
+++ MessagePrinter.java
@@ -0,0 +1,16 @@
+public class MessagePrinter {
+
+    public static String getWelcomeMessage() {
+        return "Welcome to Git and GitHub!";
+    }
+
+    public static String getDateMessage() {
+        return "Today is: " + java.time.LocalDate.now();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getWelcomeMessage());
+        System.out.println(getDateMessage());
+        System.out.println("Learning remote repositories!");
+    }
+}
